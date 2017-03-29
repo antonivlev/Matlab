8b9e81c8999fc62498483f04b94fff82d22bd88b
anton <anton.ivlev97@gmail.com>
2017-03-29 16:55:21 +0100

    sliding windows done, basic conversion to feature vecs

--- makeSlidingWindows.m
+++ makeSlidingWindows.m
@@ -1,15 +1,16 @@
-function output_map = makeSlidingWindows(data, win_size_t, overlap)
+function windows_map = makeSlidingWindows(data, win_size_t, overlap)
 %MAKESLIDINGWINDOWS(data, win_size_t, overlap) divides data into windows
 %	data - {"sensor0_data.txt", "sensor1_data.txt" ...}
-% 		file format: 	x0 	y0  z0 ... t0
+% 		file format in listed files:
+%						x0 	y0  z0 ... t0
 %						x1 	y1	z1 ... t1
 %						...
 % 	win_size_t - size of window in seconds
 % 	overlap - percentage of window overlap
 %
 %returns:
-%	cell arr {
-%		1: map {
+%	map {
+%		1 (window_index): map {
 %				"sensor0_data.txt":
 %					matrix where columns are dimensions
 %					rows are samples in window, like:
@@ -22,26 +23,47 @@ function output_map = makeSlidingWindows(data, win_size_t, overlap)
 %				...
 %			},
 %      	... ,
-%      	last_window: {}
+%      	last_window: {...}
 %	}
-%sensor vals are in the same order as filenames in data
 %
-windows_map = containers.Map({1}, {containers.Map()});
+	%Initialise map to contain windows
+	windows_map = containers.Map({1}, {containers.Map()});
 
-for i=1:length(data)
-	file_name = char(data(i));
-	data_mat = load(strcat('Data/', file_name), '-ascii');
-	disp(strcat('loaded ', file_name));
-	data_mat = removeZeroTimes(data_mat);
-	tvals = data_mat(:, end);
-	%Timestamp of first window end
-	t1 = min(tvals) + win_size_t*1000;
-	disp(strcat('----t1:', string(t1)));
-	%Width of the window in points
-	win_width = getPointsWindow(t1, tvals);
-	disp(strcat('---- width:', string(win_width), ' points'));
-end
+	%for each data file
+	for i=1:length(data)
+		file_name = char(data(i));
+		data_mat = load(strcat('Data/', file_name), '-ascii');
 
+		fprintf('loaded: %s\n', file_name);
+		data_mat = removeZeroTimes(data_mat);
+		tvals = data_mat(:, end);
+
+		%Timestamp of first window end
+		t1 = min(tvals) + win_size_t*1000;
+		fprintf('	t1: %d\n', t1);
+		%Width of the window in points
+		win_width = getPointsWindow(t1, tvals);
+		fprintf('	window_width: %d points\n', win_width);
+
+		start_p = 1;
+		window_index = 1;
+		%for each widow in current data file
+		while start_p + win_width < length(tvals)
+			window = data_mat 	(start_p : start_p+win_width-1, ...
+								1 : end-1);
+			start_p = start_p + (win_width-win_width*(overlap/100));
+
+			if (i == 1)
+				sensors_map = containers.Map();
+				windows_map(window_index) = sensors_map;
+			end
+			smap = windows_map(window_index);
+			smap(file_name) = window;
+
+			window_index = window_index+1;
+		end
+		fprintf('	num_windows: %d\n', window_index);
+	end
 end
 
 function ind = getPointsWindow(t, tcol)
